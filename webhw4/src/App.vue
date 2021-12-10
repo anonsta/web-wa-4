a4754a9196728f1c84f83d06dc3e3c92ba3534fa
Ats Tragel <atragel@gmail.com>
2021-12-10 15:11:16 +0200

    project files

---
+++ webhw4/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+const express = require('express');
+const pool = require('./database');
+const app = express();
+// register the ejs view engine
+app.set('view engine', 'ejs');
+app.listen(3000);
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
